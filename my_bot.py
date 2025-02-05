358b4b828fd4d5e41786b1458f2bee46facea210
Stella Talcott <stalcott2027@kehillahstudent.org>
2025-02-04 19:59:17 -0800

    1 response implemented

--- my_bot.py
+++ my_bot.py
@@ -8,7 +8,7 @@ This function will be called every time anyone says anything on a channel where 
 * You can have the bot respond differently to different users
 """
 def should_i_respond(user_message, user_name):
-  if "robot" in user_message:
+  if "repeat after me" in user_message:
     return True
   else:
     return False
@@ -22,6 +22,21 @@ This function will be called every time the `should_i_respond` function returns 
 * The bot will post the returned string on the channel where the original message was sent.
 * You can have the bot respond differently to different messages and users
 """
+# def respond(user_message, user_name):
+#   return f"""you said my name!!
+#   {user_message.replace("robot", user_name)}"""
+
+
 def respond(user_message, user_name):
-  return f"""you said my name!!
-  {user_message.replace("robot", user_name)}"""
+  return alt_cap(user_message.replace("repeat after me: ", ""))
+
+
+def alt_cap(text):
+    new_string = ""
+    for i in range(len(text)):
+        character = text[i]
+        if i%2 == 0:
+            new_string = new_string + character.upper()
+        else:
+            new_string = new_string + character.lower()
+    return new_string
