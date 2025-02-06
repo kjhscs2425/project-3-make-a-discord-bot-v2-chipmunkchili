a4dc6e768375c11dc5de28a9a1abe02ce12041c3
Stella Talcott <stalcott2027@kehillahstudent.org>
2025-02-05 18:27:38 -0800

    5 responses implemented

--- main.py
+++ main.py
@@ -22,8 +22,38 @@ async def on_message(message):
   if message.channel.name == "stella_bot":
     if message.author != client.user:
       user_name = message.author.display_name
-      if my_bot.should_i_respond(message.content, user_name):
-        response = my_bot.respond(message.content, user_name)
+      if my_bot.should_i_respond_1(message.content, user_name):
+        response = my_bot.respond_1(message.content, user_name)
+        await message.channel.send(response)
+      elif my_bot.should_i_respond_2(message.content, user_name):
+        response = my_bot.respond_2(message.content, user_name)
+        await message.channel.send(response)
+      elif my_bot.should_i_respond_3(message.content, user_name):
+        response = my_bot.respond_3(message.content, user_name)
+        await message.channel.send(response)
+      elif my_bot.should_i_respond_4(message.content, user_name):
+        response = my_bot.respond_4(message.content, user_name)
+        await message.channel.send(response)
+      elif my_bot.should_i_respond_5(message.content, user_name):
+        response = my_bot.respond_5(message.content, user_name)
+        await message.channel.send(response)
+      # elif my_bot.should_i_respond_6(message.content, user_name):
+      #   response = my_bot.respond_6(message.content, user_name)
+      #   await message.channel.send(response)
+      # elif my_bot.should_i_respond_7(message.content, user_name):
+      #   response = my_bot.respond_7(message.content, user_name)
+      #   await message.channel.send(response)
+      # elif my_bot.should_i_respond_8(message.content, user_name):
+      #   response = my_bot.respond_8(message.content, user_name)
+      #   await message.channel.send(response)
+      # elif my_bot.should_i_respond_9(message.content, user_name):
+      #   response = my_bot.respond_9(message.content, user_name)
+      #   await message.channel.send(response)
+      # elif my_bot.should_i_respond_10(message.content, user_name):
+      #   response = my_bot.respond_10(message.content, user_name)
+      #   await message.channel.send(response)
+      else:
+        response = "What did you say?"
         await message.channel.send(response)
 
 client.run(my_secret)
--- my_bot.py
+++ my_bot.py
@@ -1,3 +1,5 @@
+import random
+
 """
 **Do NOT change the name of this function.**
 
@@ -7,11 +9,11 @@ This function will be called every time anyone says anything on a channel where 
 * You can have certain words or patterns in the messages trigger the bot.
 * You can have the bot respond differently to different users
 """
-def should_i_respond(user_message, user_name):
-  if "repeat after me" in user_message:
-    return True
-  else:
-    return False
+# def should_i_respond(user_message, user_name):
+#   if "robot" in user_message:
+#     return True
+#   else:
+#     return False
 
 """
 **Do NOT change the name of this function.**
@@ -27,9 +29,15 @@ This function will be called every time the `should_i_respond` function returns 
 #   {user_message.replace("robot", user_name)}"""
 
 
-def respond(user_message, user_name):
-  return alt_cap(user_message.replace("repeat after me: ", ""))
+# REPEAT IN ALTERNATING CAPS
+def should_i_respond_1(user_message, user_name):
+  if "repeat after me" in user_message:
+    return True
+  else:
+    return False
 
+def respond_1(user_message, user_name):
+  return alt_cap(user_message.replace("repeat after me: ", ""))
 
 def alt_cap(text):
     new_string = ""
@@ -40,3 +48,48 @@ def alt_cap(text):
         else:
             new_string = new_string + character.lower()
     return new_string
+
+
+# TELL ME A JOKE
+def should_i_respond_2(user_message, user_name):
+  if "tell me a joke" in user_message:
+    return True
+  else:
+    return False
+
+def respond_2(user_message, user_name):
+  return "Why was the robot couple's anniversary in the fall?\nThey were autumn-mated!"
+
+
+# RANDOM COLOR
+def should_i_respond_3(user_message, user_name):
+  if "color" in user_message:
+    return True
+  else:
+    return False
+
+def respond_3(user_message, user_name):
+  color = random.choice(["black", "brown", "grey", "white", "red", "orange", "yellow", "green", "blue", "purple", "pink"])
+  return color
+
+
+# HI MY NAME IS
+def should_i_respond_4(user_message, user_name):
+  if "hi my name is" in user_message:
+    return True
+  else:
+    return False
+
+def respond_4(user_message, user_name):
+  return f'Hi, {user_message.replace("hi my name is ", "")}, my name is robot!'
+
+
+# POTATO
+def should_i_respond_5(user_message, user_name):
+  if "potato" in user_message:
+    return True
+  else:
+    return False
+
+def respond_5(user_message, user_name):
+  return "I LOVE POTATOES! There are so many wants to eat them."
