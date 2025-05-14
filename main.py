506277699298d83e69ee7efbd193fb4814c0b061
Stella Talcott <stalcott2027@kehillahstudent.org>
2025-05-14 12:12:30 -0700

    change

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.channel.name == "stella_bot":
+  #if message.channel.name == "stella_bot":
     if message.author != client.user:
       user_name = message.author.display_name
       response = my_bot.respond(message.content, user_name)
